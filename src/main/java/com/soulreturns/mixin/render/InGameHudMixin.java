4444303a1fd007222d459684908c45aabdc21629
SoulReturns <pascal.hannemann@gmail.com>
2025-11-13 15:41:18 +0100

    Feature: hide vanilla held item tooltips.

---
+++ src/main/java/com/soulreturns/mixin/render/InGameHudMixin.java
@@ -0,0 +1,20 @@
+package com.soulreturns.mixin.render;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    @Inject(
+            method = "renderHeldItemTooltip",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void renderHeldItemTooltip(DrawContext context, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
