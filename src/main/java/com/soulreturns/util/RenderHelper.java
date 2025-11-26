bd02928e4c3d2f42e29d969628d3e16f36ac309b
Soul-Returns <Soul-Returns@users.noreply.github.com>
2025-11-26 13:01:13 +0100

    refactor RenderHelper for compatibility with Minecraft version 1.21.5

--- src/main/java/com/soulreturns/util/RenderHelper.java
+++ src/main/java/com/soulreturns/util/RenderHelper.java
@@ -3,7 +3,11 @@ package com.soulreturns.util;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
+//? if >=1.21.8 {
 import org.joml.Matrix3x2fStack;
+//?} else {
+/*import net.minecraft.client.util.math.MatrixStack;
+*///?}
 
 public class RenderHelper {
     /**
@@ -13,7 +17,11 @@ public class RenderHelper {
     public static void drawScaledText(DrawContext context, TextRenderer textRenderer,
                                      Text text, int centerX, int centerY,
                                      float scale, int color) {
+        //? if >=1.21.8 {
         Matrix3x2fStack matrices = context.getMatrices();
+        //?} else {
+        /*MatrixStack matrices = context.getMatrices();
+        *///?}
 
         int textWidth = textRenderer.getWidth(text);
 
@@ -25,14 +33,23 @@ public class RenderHelper {
         float y = centerY / scale;
 
         // Save matrix state and apply scaling
+        //? if >=1.21.8 {
         matrices.pushMatrix();
         matrices.scale(scale, scale);
+        //?} else {
+        /*matrices.push();
+        matrices.scale(scale, scale, 1.0f);
+        *///?}
 
         // Draw text at the scaled position
         context.drawTextWithShadow(textRenderer, text, (int)x, (int)y, color);
 
         // Restore matrix state
+        //? if >=1.21.8 {
         matrices.popMatrix();
+        //?} else {
+        /*matrices.pop();
+        *///?}
     }
 }
 
