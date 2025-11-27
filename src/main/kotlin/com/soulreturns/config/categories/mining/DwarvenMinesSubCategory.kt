4256f70252f47cf8aefac56b476be7f52acb96c9
Soul-Returns <Soul-Returns@users.noreply.github.com>
2025-11-27 15:29:25 +0100

    add info to Don Expresso feature.

--- src/main/kotlin/com/soulreturns/config/categories/mining/DwarvenMinesSubCategory.kt
+++ src/main/kotlin/com/soulreturns/config/categories/mining/DwarvenMinesSubCategory.kt
@@ -7,7 +7,7 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 class DwarvenMinesSubCategory {
     @Expose
     @JvmField
-    @ConfigOption(name = "Don Expresso", desc = "Show an alert when Don Expresso is about to leave")
+    @ConfigOption(name = "Don Expresso", desc = "Show an alert when Don Expresso is about to leave (currently only works when completed early)")
     @ConfigEditorBoolean
     var donExpressoAlert: Boolean = true
 }
